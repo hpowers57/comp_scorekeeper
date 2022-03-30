5bcd8f7ffcc93e24dd527a73250159704e13757c
Hannah Powers <hannahpowers57@gmail.com>
2022-03-30 18:10:29 -0400

    Updatesssss

--- gui.py
+++ gui.py
@@ -1,10 +1,158 @@
+import os.path
 import traceback
 
+import numpy as np
+import pandas as pd
 from PyQt5.QtWidgets import *
 from time import time
 from PyQt5.QtGui import QPixmap
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 from scorecard import *
+import json
+
+
+class placingResultsWindow(QWidget):
+    def __init__(self, main, results, event_no, round, dance):
+        super().__init__()
+        self.main = main
+        self.event_no = event_no
+        self.round = round
+        self.dance = dance
+        self.results, self.judges = results
+        round_word = round == "QF" or round == "SF" or round == "Final"
+        title = QLabel("Places for event #{}: {} {}".format(event_no, dance,
+                                                            round if round_word else round + str(" Round")))
+        title.setStyleSheet("font-weight: bold")
+
+        n = 10 - np.count_nonzero(pd.isna(self.results["Couple"]))
+        self.pretty = self.results.sort_values(by=self.judges[0])
+        self.pretty["Place"] = self.pretty["Place"].astype(str)
+        for i in range(1, n + 1):
+            self.pretty["1-" + str(i)] = self.pretty["1-" + str(i)].astype(int).astype(str) + "(" + self.pretty["1-" + str(i) + "SUM"].astype(
+                int).astype(str) + ")"
+            self.pretty.loc[self.pretty["1-" + str(i)] == "0(0)", "1-" + str(i)] = ""
+        for i in range(1, n + 1):
+            self.pretty.loc[self.pretty["Place"] == str(i), ["1-" + str(j) for j in range(i + 1, n + 1)]] = ""
+        drop = [[str(i) + "_", "1-" + str(i), "1-" + str(i) + "SUM"] for i in range(n + 1, 11)]
+        drop = [elmt for sublst in drop for elmt in sublst] + [str(i) + "_" for i in range(1, n + 1)] + [
+            "1-" + str(i) + "SUM" for i in range(1, n + 1)]
+        self.pretty = self.pretty.loc[pd.isna(self.pretty["Couple"]) != True].drop(drop, axis=1)
+
+        self.places = QTableWidget(self.pretty.shape[0], self.pretty.shape[1])
+        cols = list(self.pretty.columns)
+        self.places.setHorizontalHeaderLabels(cols)
+        self.places.setColumnWidth(0, 70)
+        self.places.setColumnWidth(self.pretty.shape[1] - 1, 50)
+        for i in range(1, len(self.judges) + 1):
+            self.places.setColumnWidth(i, 50)
+        for i in range(len(self.judges) + 1, self.pretty.shape[1] - 1):
+            self.places.setColumnWidth(i, 70)
+        for i in range(self.pretty.shape[0]):
+            for j in range(self.pretty.shape[1]):
+                itm = QTableWidgetItem(str(self.pretty[cols[j]].values[i]))
+                itm.setForeground(QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+                itm.setFlags(QtCore.Qt.ItemIsEditable)
+                self.places.setItem(i, j, itm)
+
+        layout = QVBoxLayout()
+        layout.addWidget(title)
+        layout.addWidget(self.places)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Comp Scorekeeper")
+        self.setMinimumSize(800, 410)
+
+    def closeEvent(self, event):
+        self.main.windows.remove(self)
+        super().closeEvent(event)
+
+
+class callbacksResultsWindow(QWidget):
+    def __init__(self, main, results, event_no, round, dance):
+        super().__init__()
+        self.main = main
+        self.event_no = event_no
+        self.round = round
+        self.dance = dance
+        self.results, self.judges = results
+        self.n = len(self.results)
+        m = 1 + len(self.judges)
+        round_word = round == "QF" or round == "SF" or round == "Final"
+        title = QLabel("Places for event #{}: {} {}".format(event_no, dance,
+                                                            round if round_word else round + str(" Round")))
+
+        num_label = QLabel("Please select a number of callbacks needed:")
+        self.num = QSpinBox()
+        self.num.setRange(0, len(self.judges))
+        self.num.setValue(np.ceil(len(self.judges) / 2))
+        self.num.valueChanged.connect(self.changeCallbacks)
+
+        self.num_callbacks = dict()
+        for i in range(len(self.judges) + 1):
+            self.num_callbacks[i] = sum([1 if len(y) >= i else 0 for _, y in self.results])
+        num_called_label = QLabel("Number of couples called:")
+        self.num_called = QLineEdit()
+        self.num_called.setText(str(self.num_callbacks[self.num.value()]))
+        self.num_called.setReadOnly(True)
+
+        self.callbacks = QTableWidget(self.n, m)
+        for i in range(1, m):
+            self.callbacks.setColumnWidth(i, 50)
+        cols = ["Couple"] + self.judges
+        self.callbacks.setHorizontalHeaderLabels(cols)
+        for i in range(self.n):
+            call = len(self.results[i][1]) >= self.num.value()
+            itm = QTableWidgetItem(self.results[i][0])
+            itm.setFlags(QtCore.Qt.ItemIsEditable)
+            itm.setForeground(QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+            if call:
+                itm.setBackground(QtGui.QColor(200, 200, 200))
+            self.callbacks.setItem(i, 0, itm)
+            for j in range(1, m):
+                if cols[j] in self.results[i][1]:
+                    itm = QTableWidgetItem("X")
+                    itm.setFlags(QtCore.Qt.ItemIsEditable)
+                    itm.setForeground(QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+                    if call:
+                        itm.setBackground(QtGui.QColor(200, 200, 200))
+                    self.callbacks.setItem(i, j, itm)
+                else:
+                    itm = QTableWidgetItem("")
+                    itm.setFlags(QtCore.Qt.ItemIsEditable)
+                    itm.setForeground(QtGui.QBrush(QtGui.QColor(0, 0, 0)))
+                    if call:
+                        itm.setBackground(QtGui.QColor(200, 200, 200))
+                    self.callbacks.setItem(i, j, itm)
+
+        layout = QVBoxLayout()
+        layout.addWidget(title)
+        layout2 = QHBoxLayout()
+        layout2.addWidget(num_label)
+        layout2.addWidget(self.num)
+        layout.addLayout(layout2)
+        layout3 = QHBoxLayout()
+        layout3.addWidget(num_called_label)
+        layout3.addWidget(self.num_called)
+        layout.addLayout(layout3)
+        layout.addWidget(self.callbacks)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Comp Scorekeeper")
+        self.setMinimumSize(200, 500)
+
+    def changeCallbacks(self):
+        self.num_called.setText(str(self.num_callbacks[self.num.value()]))
+        for i in range(self.n):
+            call = len(self.results[i][1]) >= self.num.value()
+            for j in range(len(self.judges) + 1):
+                if call:
+                    self.callbacks.item(i, j).setBackground(QtGui.QColor(200, 200, 200))
+                else:
+                    self.callbacks.item(i, j).setBackground(QtGui.QColor(255, 255, 255))
+
+    def closeEvent(self, event):
+        self.main.windows.remove(self)
+        super().closeEvent(event)
 
 
 class CallbacksWindow(QWidget):
@@ -133,7 +281,7 @@ class PlacingWindow(QWidget):
         for i in range(10):
             tmp = self.places.item(i, 0).text()
             if tmp != '' and tmp.isnumeric():
-                places[j] = tmp
+                places[tmp] = j
                 j += 1
         if judge != "" and judge.isnumeric() and len(places.keys()) > 0:
             self.main.saveScoresheet(judge, places, (self.event_no, self.round, self.dance), self)
@@ -148,20 +296,23 @@ class PlacingWindow(QWidget):
 
 
 class RoundWindow(QWidget):
-    windows = list()
-    scoresheets_list = dict()
-
     def __init__(self, main):
         super().__init__()
+        self.windows = list()
+        self.scoresheets_list = dict()
+        self.callbacks = Callbacks()
+        self.places = Places()
         self.main = main
 
         layoutLeft = QVBoxLayout()
         instr = QLabel("State the event number, round, and dance, then add some scoresheets!")
         event_no_label = QLabel("Event Number")
         event_no_label.setStyleSheet("font-weight: bold")
-        self.event_no = QLineEdit()
-        self.event_no.setPlaceholderText("0")
+        self.event_no = QSpinBox()
+        self.event_no.setValue(0)
+        self.event_no.setRange(0, 500)
         self.event_no.setFixedWidth(100)
+        self.event_no.valueChanged.connect(self.eventNoChanged)
         round_label = QLabel("Round")
         round_label.setStyleSheet("font-weight: bold")
         self.round = QComboBox()
@@ -173,6 +324,7 @@ class RoundWindow(QWidget):
         self.round.addItem("SF")
         self.round.addItem("Final")
         self.round.setFixedWidth(100)
+        self.round.currentTextChanged.connect(self.roundChanged)
         dance_label = QLabel("Dance")
         dance_label.setStyleSheet("font-weight: bold")
         self.dance = QComboBox()
@@ -192,6 +344,7 @@ class RoundWindow(QWidget):
         self.dance.addItem("Jive")
         self.dance.addItem("Paso Doble")
         self.dance.setFixedWidth(100)
+        self.dance.currentTextChanged.connect(self.danceChanged)
         blank = QLabel("")
         cancel = QPushButton("Cancel")
         cancel.setFixedWidth(100)
@@ -216,12 +369,16 @@ class RoundWindow(QWidget):
         miniLayout = QHBoxLayout()
         miniLayout.addWidget(clear)
         miniLayout.addWidget(delete)
+        calculate = QPushButton("Calculate")
+        calculate.clicked.connect(self.calculateRound)
         save = QPushButton("Save round")
+        save.clicked.connect(self.saveRound)
         self.scoresheets = QListWidget()
         self.scoresheets.itemDoubleClicked.connect(self.reopenScoresheet)
         layoutRight.addWidget(add)
         layoutRight.addWidget(self.scoresheets)
         layoutRight.addLayout(miniLayout)
+        layoutRight.addWidget(calculate)
         layoutRight.addWidget(save)
 
         layout = QHBoxLayout()
@@ -232,24 +389,114 @@ class RoundWindow(QWidget):
         self.setWindowTitle("Comp Scorekeeper")
         self.setMinimumSize(500, 500)
 
+    def eventNoChanged(self):
+        judges = list(self.scoresheets_list.keys())
+        for j in judges:
+            scores, event_info = self.scoresheets_list[j]
+            self.scoresheets_list[j] = (scores, (str(self.event_no.value()), event_info[1], event_info[2]))
+
+    def danceChanged(self):
+        judges = list(self.scoresheets_list.keys())
+        for j in judges:
+            scores, event_info = self.scoresheets_list[j]
+            self.scoresheets_list[j] = (scores, (event_info[0], event_info[1], self.dance.currentText()))
+
+    def roundChanged(self):
+        if len(self.scoresheets_list.keys()) > 0:
+            judges = list(self.scoresheets_list.keys())
+            scores, event_info = self.scoresheets_list[judges[0]]
+            if (event_info[1] == "Final" and self.round.currentText() != "Final") or (event_info[1] != "Final" and self.round.currentText() == "Final"):
+                alert = QMessageBox()
+                alert.setText("You changed this round to or from a final and your scoresheets don't match! Changing will remove all scoresheets. What would you like to do?")
+                alert.addButton("Change anyway", QMessageBox.YesRole)
+                alert.addButton("Cancel", QMessageBox.NoRole)
+                ret = alert.exec_()
+                if ret == 0:
+                    self.clearScoresheets()
+                else:
+                    self.round.setCurrentText(event_info[1])
+            else:
+                for j in judges:
+                    scores, event_info = self.scoresheets_list[j]
+                    self.scoresheets_list[j] = (scores, (event_info[0], self.round.currentText(), event_info[2]))
+
+    def saveRound(self):
+        event_no = self.event_no.value()
+        round = self.round.currentText()
+        dance = self.dance.currentText()
+        if event_no == 0 or round == "" or dance == "":
+            alert = QMessageBox()
+            alert.setText("Try adding in an event number, round, and dance before saving!")
+            alert.exec_()
+        else:
+            title = "saved/event{}_{}_{}_round_data.json".format(event_no, dance, round)
+            tmp = {"event_no": event_no, "round": round, "dance": dance, "scoresheets": self.scoresheets_list}
+            with open(title, "w", encoding="utf-8") as f:
+                json.dump(tmp, f, ensure_ascii=False, indent=4)
+
+            if os.path.isfile(title):
+                alert = QMessageBox()
+                alert.setText("Round saved! Would you like to keep working on it or close it?")
+                alert.addButton("Keep working", QMessageBox.YesRole)
+                alert.addButton("Close", QMessageBox.NoRole)
+                ret = alert.exec_()
+                if ret != 0:
+                    self.close()
+            else:
+                alert = QMessageBox()
+                alert.setText("Failed to save round!")
+                alert.exec_()
+
+    def calculateRound(self):
+        try:
+            if self.round.currentText() == "Final":
+                scores = self.places.return_places()
+                func = placingResultsWindow
+            else:
+                scores = self.callbacks.return_callbacks()
+                func = callbacksResultsWindow
+            if scores is None:
+                alert = QMessageBox()
+                alert.setText("Not enough scoresheets! Callbacks must have at least one and placing must have an odd number.")
+                alert.exec_()
+            else:
+                window = func(self, scores, str(self.event_no.value()), self.round.currentText(), self.dance.currentText())
+                self.windows.append(window)
+                window.show()
+        except Exception:
+            print(traceback.format_exc())
+
     def saveScoresheet(self, judge, scores, event_info, scoresheet):
         def helper():
-            # TODO: add in saving using classes from scorecard module
-            item = QListWidgetItem("Judge {}".format(judge))
-            match = self.scoresheets.findItems("Judge {}".format(judge), QtCore.Qt.MatchExactly)
-            if len(match) != 0:
-                for m in match:
-                    idx = self.scoresheets.indexFromItem(m).row()
-                    self.scoresheets.takeItem(idx)
-            self.scoresheets.addItem(item)
-            self.scoresheets_list[judge] = (scores, event_info)
+            if self.round.currentText() == "Final":
+                try:
+                    ret = self.places.add_places(judge, scores)
+                except Exception:
+                    print(traceback.format_exc())
+            else:
+                self.callbacks.add_callbacks(judge, scores)
+                ret = True
+            if not ret:
+                alert = QMessageBox()
+                alert.setText("You're trying to place too many couples! Check your scoresheets to make sure the numbers are correct.")
+                alert.exec_()
+            else:
+                item = QListWidgetItem("Judge {}".format(judge))
+                match = self.scoresheets.findItems("Judge {}".format(judge), QtCore.Qt.MatchExactly)
+                if len(match) != 0:
+                    for m in match:
+                        idx = self.scoresheets.indexFromItem(m).row()
+                        self.scoresheets.takeItem(idx)
+                self.scoresheets.addItem(item)
+                self.scoresheets_list[judge] = (scores, event_info)
+                scoresheet.close()
 
         if (event_info[1] == "Final" and self.round.currentText() != "Final") or (event_info[1] != "Final" and self.round.currentText() == "Final"):
             alert = QMessageBox()
             alert.setText("Scoresheet's round type does not match event's round type! Could not save scoresheet.")
             alert.exec_()
             scoresheet.close()
-        elif event_info[0] != self.event_no.text() or event_info[2] != self.dance.currentText() or event_info[1] != self.round.currentText():
+        elif event_info[0] != str(self.event_no.value()) or event_info[2] != self.dance.currentText() or event_info[1] != self.round.currentText():
             alert = QMessageBox()
             alert.setText("Event number, dance, or round does not match event's. What would you like to do?")
             alert.addButton("Save anyway", QMessageBox.YesRole)
@@ -262,7 +509,6 @@ class RoundWindow(QWidget):
                 scoresheet.close()
         else:
             helper()
-            scoresheet.close()
 
     def reopenScoresheet(self, item: QListWidgetItem):
         judge = item.text().split()[1]
@@ -271,16 +517,13 @@ class RoundWindow(QWidget):
             window = PlacingWindow(self, event_info[0], event_info[1], event_info[2])
         else:
             window = CallbacksWindow(self, event_info[0], event_info[1], event_info[2])
-        print("Here!")
         window.addScores(scores)
-        print("Here!!")
         window.addJudge(judge)
-        print("Here!!!")
         self.windows.append(window)
         window.show()
 
     def addScoresheet(self):
-        event_no = self.event_no.text()
+        event_no = str(self.event_no.value())
         round_no = self.round.currentText()
         dance = self.dance.currentText()
 
@@ -301,10 +544,14 @@ class RoundWindow(QWidget):
         sel = self.scoresheets.selectedItems()
         for s in sel:
             self.scoresheets_list.pop(s.text().split()[1])
+            self.callbacks.remove_callbacks(s.text().split()[1])
+            self.places.remove_places(s.text().split()[1])
             idx = self.scoresheets.indexFromItem(s).row()
             self.scoresheets.takeItem(idx)
 
     def clearScoresheets(self):
+        self.places.clear()
+        self.callbacks.clear()
         self.scoresheets_list.clear()
         self.scoresheets.clear()
 
@@ -345,9 +592,12 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(500, 500)
 
     def on_new_round_clicked(self):
-        round = RoundWindow(self)
-        self.windows.append(round)
-        round.show()
+        try:
+            round = RoundWindow(self)
+            self.windows.append(round)
+            round.show()
+        except Exception:
+            print(traceback.format_exc())
 
     def closeEvent(self, event):
         start = time()
@@ -362,7 +612,9 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication([])
     app.setStyleSheet("QLabel{font-size: 12pt;} QPushButton{font-size: 12pt;} " +
-                      "QComboBox{font-size: 12pt;} QLineEdit{font-size: 12pt;}")
+                      "QComboBox{font-size: 12pt;} QLineEdit{font-size: 12pt;} " +
+                      "QSpinBox{font-size: 12pt;} QTableWidget{font-size: 12pt;} " +
+                      "QListWidget{font-size: 12pt;}")
 
     window = MainWindow()
     window.show()
--- scorecard.py
+++ scorecard.py
@@ -13,11 +13,10 @@ class Callbacks:
         self.heatlist = set(hlist) if hlist else set()
         self.callbackcards = dict()
         self.overall = None
-        self.current = True
         if jlist:
             for j in jlist:
                 self.judges.append(j)
-                self.callbackcards[j] = CallbackCard(j, hlist)
+                self.callbackcards[j] = CallbackCard(j)
 
     def add_callbacks(self, judge, cblist):
         """
@@ -31,30 +30,44 @@ class Callbacks:
             self.callbackcards[judge] = CallbackCard(judge)
         self.heatlist = self.heatlist.union(cblist)
         self.callbackcards[judge].add_callbacks(cblist)
-        self.current = False
+        self.overall = None
+
+    def remove_callbacks(self, judge):
+        if judge in self.judges:
+            self.judges.remove(judge)
+            self.callbackcards.pop(judge)
+            self.overall = None
 
     def total_callbacks(self):
         """
         Updates callbacks per couple from judges callbacks cards
         """
-        self.overall = dict()
-        for j in self.judges:
-            tmp = self.callbackcards[j].callbacks
-            for num in tmp.keys():
-                if num in self.overall.keys():
-                    self.overall[num] += int(tmp[num])
-                else:
-                    self.overall[num] = int(tmp[num])
-        self.current = True
+        if len(self.judges) > 0:
+            self.overall = dict()
+            for j in self.judges:
+                tmp = self.callbackcards[j].callbacks
+                for num in tmp:
+                    if num in self.overall.keys():
+                        self.overall[num].append(j)
+                    else:
+                        self.overall[num] = [j]
 
     def return_callbacks(self):
-        if not self.current or not self.overall:
+        if self.overall is None:
             self.total_callbacks()
-        return sorted(self.overall.items(), key=lambda x: (x[1], x[0]), reverse=True)
+        if self.overall is None:
+            return None
+        return sorted(self.overall.items(), key=lambda x: (len(x[1]), x[0]), reverse=True), self.judges
+
+    def clear(self):
+        for judge in self.judges:
+            self.judges.remove(judge)
+            self.callbackcards.pop(judge)
+            self.overall = None
 
 
 class CallbackCard:
-    def __init__(self, no, heatlist=None):
+    def __init__(self, no):
         """
         Labels scorecard with judge's id info and heatlist
 
@@ -62,10 +75,7 @@ class CallbackCard:
         :param heatlist: list of competing couples
         """
         self.judge_no = no
-        self.callbacks = dict()
-        for num in heatlist:
-            self.callbacks[num] = False
-
+        self.callbacks = None
 
     def OLD_add_callbacks(self, cblist):
         """
@@ -89,8 +99,7 @@ class CallbackCard:
 
         :param pdict: list of couple callbacks
         """
-        for num in cblist:
-            self.callbacks[num] = True
+        self.callbacks = set(cblist)
 
 
 class Places:
@@ -102,10 +111,9 @@ class Places:
         :param hlist: list of couples
         """
         self.judges = list()
-        self.heatlist = set(hlist) if hlist else set()
         self.placingcards = dict()
         self.overall = None
-        self.current = True
+        self.heatlist = set()
         if jlist:
             for j in jlist:
                 self.judges.append(j)
@@ -118,12 +126,25 @@ class Places:
         :param judge: string of judge number
         :param pdict: dict of placement by couple
         """
+        if len(self.heatlist.union(pdict.keys())) > 8:
+            return False
         if judge not in self.judges:
             self.judges.append(judge)
             self.placingcards[judge] = PlacingCard(judge)
-        self.heatlist = self.heatlist.union(pdict.keys())
         self.placingcards[judge].add_places(pdict)
-        self.current = False
+        self.overall = None
+        self.heatlist = self.heatlist.union(pdict.keys())
+        return True
+
+    def remove_places(self, judge):
+        if judge in self.judges:
+            self.judges.remove(judge)
+            self.placingcards.pop(judge)
+            self.overall = None
+
+        self.heatlist = set()
+        for judge in self.judges:
+            self.heatlist = self.heatlist.union(self.placingcards[judge].places.keys())
 
     def calculate_places(self):
         """
@@ -141,7 +162,10 @@ class Places:
             self.overall = pd.DataFrame(columns=columns, index=list(range(10)))
 
             # Fill couples and judge placements
-            self.overall["Couple"] = list(self.heatlist) + [np.NaN] * (10 - len(self.heatlist))
+            heatlist = set()
+            for j in self.judges:
+                heatlist = heatlist.union(self.placingcards[j].places.keys())
+            self.overall["Couple"] = list(heatlist) + [np.NaN] * (10 - len(heatlist))
             for j in self.judges:
                 idx = [couple in self.placingcards[j].places.keys() for couple in self.overall["Couple"]]
                 self.overall.loc[idx, j] = [self.placingcards[j].places[couple] for couple in self.overall.loc[idx, "Couple"]]
@@ -157,7 +181,9 @@ class Places:
                 self.overall["1-" + str(i) + "SUM"] = self.overall["1-" + str(i - 1) + "SUM"] + (self.overall[str(i) + "_"] * i)
 
             # Decide placements
-            for i in range(1, 11):
+            # TODO: ask Matt if we should break ties after first attempt or just award
+            i = 1
+            while i < 11:
                 idxna = pd.isna(self.overall["Place"])
                 couples = self.overall.loc[idxna, "Couple"]
                 idxp = (self.overall.loc[idxna, "1-" + str(i)] == max(self.overall.loc[idxna, "1-" + str(i)]))
@@ -167,13 +193,10 @@ class Places:
                         self.overall.loc[idxna].loc[idxp, "1-" + str(i) + "SUM"])
                     couples = couples.loc[idxs]
                 idxf = [couple in couples.values for couple in self.overall["Couple"]]
-                tmp = [i for j in range(np.count_nonzero(couples.shape[0]))]
+                n = np.count_nonzero(idxf)
+                tmp = [i for _ in range(n)]
                 self.overall.loc[idxf, "Place"] = tmp
-        else:
-            return False
-
-        self.current = True
-        return True
+                i += n if n > 0 else 1
 
     def return_places(self):
         """
@@ -181,9 +204,19 @@ class Places:
 
         :return: dataframe with couples, calculation table, and placement by judge and overall
         """
-        if not self.current or not self.overall:
+        if self.overall is None:
             self.calculate_places()
-        return self.overall.copy()
+        if self.overall is None:
+            return None
+        return self.overall.copy(), self.judges
+
+    def clear(self):
+        for judge in self.judges:
+            self.judges.remove(judge)
+            self.placingcards.pop(judge)
+            self.overall = None
+
+        self.heatlist = set()
 
 
 class PlacingCard:
@@ -231,12 +264,29 @@ class PlacingCard:
 
 
 if __name__ == "__main__":
-    places = {"100": 1, "101": 2, "112": 3, "113": 4, "114": 5, "117": 6, "120": 7, "135": 8}
-    places1 = {"100": 3, "101": 1, "112": 2, "113": 4, "114": 8, "117": 5, "120": 7, "135": 6}
-    places2 = {"100": 1, "101": 4, "112": 2, "113": 5, "114": 3, "117": 6, "120": 8, "135": 7}
-    p = {"10": places, "11": places1, "12": places2}
+    # round = Callbacks()
+    # j1 = ["100", "101", "112", "113", "114", "117"]
+    # j2 = ["112", "113", "114", "117", "120", "135"]
+    # j3 = ["100", "112", "117", "135", "141", "171"]
+    # round.add_callbacks("10", j1)
+    # round.add_callbacks("11", j2)
+    # round.add_callbacks("12", j3)
+    # print(round.return_callbacks())
+
+    # places = {"100": 1, "101": 2, "112": 3, "113": 4, "114": 5, "117": 6, "120": 7, "135": 8}
+    # places1 = {"100": 3, "101": 1, "112": 2, "113": 4, "114": 8, "117": 5, "120": 7, "135": 6}
+    # places2 = {"100": 1, "101": 4, "112": 2, "113": 5, "114": 3, "117": 6, "120": 8, "135": 7}
+    # p = {"10": places, "11": places1, "12": places2}
+    # round = Places()
+    # round.add_places("10", places)
+    # round.add_places("11", places1)
+    # round.add_places("12", places2)
+    # tmp, judges = round.return_places()
+
     round = Places()
-    round.add_places("10", places)
-    round.add_places("11", places1)
-    round.add_places("12", places2)
-    print(round.return_places().sort_values(by="10"))
+    round.add_places("10", {"10": 1, "20": 2, "30": 3, "40": 4, "50": 5, "60": 6})
+    round.add_places("11", {"20": 1, "10": 2, "40": 3, "30": 4, "60": 5, "50": 6})
+    round.add_places("12", {"30": 1, "20": 2, "10": 3, "60": 4, "50": 5, "40": 6})
+    round.add_places("13", {"60": 1, "50": 2, "40": 3, "30": 4, "20": 5, "10": 6})
+    round.add_places("14", {"30": 1, "10": 2, "20": 3, "60": 4, "40": 5, "50": 6})
+    print(round.return_places()[0])
